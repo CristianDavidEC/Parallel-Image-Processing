d53798b8adfe8370d849d14fcae05006dd6b8058
CristianDavidEC <cristianescobarcdec@gmail.com>
2023-12-11 01:08:49 -0500

    clean folder+

--- src/dowloader/dowload_images.py
+++ src/dowloader/dowload_images.py
@@ -3,15 +3,37 @@ from tqdm import tqdm
 import os
 import requests
 import os
+import math
+import threading
+import shutil
 
 load_dotenv('../.env')
 # Configuración de la API de Pexels
 PEXELS_API_KEY = os.getenv('PEXELS_API_KEY')
+MAX_NUMBER_IMAGES = 80
+
+
+def thread_download_images(query, total_images=1000):
+    clean_contents()
+    TOTAL_THREADS = 10
+    iteration_pages = math.ceil(
+        (total_images/TOTAL_THREADS) / MAX_NUMBER_IMAGES)
+    threads = []
+    for i in range(TOTAL_THREADS):
+        thread = threading.Thread(target=dowload_images,
+                                  args=(query, total_images //
+                                        TOTAL_THREADS, i * iteration_pages),
+                                  name=f'Thread {i}')
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
 
 def dowload_images(query, number_of_images, page):
-    MAX_NUMBER_IMAGES = 80
-    progress_bar = tqdm(total=number_of_images, desc='Descargando imágenes')
+    progress_bar = tqdm(total=number_of_images,
+                        desc=f'Descargando imágenes - {threading.current_thread().name}: ')
     count_images = 0
     base_url = 'https://api.pexels.com/v1/search'
     params = {'query': query,
@@ -24,7 +46,11 @@ def dowload_images(query, number_of_images, page):
             page += 1
             params['page'] = page
 
-        response = requests.get(base_url, headers=headers, params=params)
+        try:
+            response = requests.get(base_url, headers=headers, params=params)
+        except Exception as e:
+            print(f'Error al obtener las imágenes: {e}')
+            break
 
         if response.status_code == 200:
             for photo in response.json()['photos']:
@@ -42,7 +68,21 @@ def dowload_images(query, number_of_images, page):
                     break
         else:
             print(
-                f"Error al obtener las imágenes. Código de estado: {response.status_code}")
+                f"Error al obtener las imágenes de la página {page} - {threading.current_thread().name} - {response.status_code}")
 
 
+def clean_contents():
+    path_folder = '../resources/images'
+    try:
+        shutil.rmtree(path_folder)
+        print(f"Contenido eliminado correctamente.")
+    except Exception as e:
+        print(f"No se pudo eliminar el contenido: {e}")
 
+    
+    nueva_carpeta = os.path.join(path_folder)
+    os.makedirs(nueva_carpeta)
+    print(f"Nueva carpeta creada en {path_folder}")
+
+if __name__ == '__main__':
+    thread_download_images('dog', 1000)
